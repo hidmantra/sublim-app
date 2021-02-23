1332d482dbae52d7838185c4cfe76e0c8bdf564d
Deriv Diggs <derivdiggs@gmail.com>
2021-02-22 22:09:14 -0500

    added video plugin

--- sublim/src/app/tab3/tab3.page.ts
+++ sublim/src/app/tab3/tab3.page.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { VideoPlayer, VideoOptions } from '@ionic-native/video-player/ngx';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
+
+
 export class Tab3Page {
+ // constructor(){}
+  videoOpts : VideoOptions ;
+  constructor(private videoPlayer: VideoPlayer) {
 
-  constructor() {}
-
+  }
+  public playVideo(){
+    this.videoOpts = {volume : 1.0};
+    this.videoPlayer.play('file:///android_asset/www/cat.mp4').then(() => {
+    console.log('video completed');
+    }).catch(err => {
+    console.log(err);
+    });    
+}
+public stopPlayingVideo(){
+    this.videoPlayer.close();
+}
 }
